9d1314ee9638677c0cdb05301669439d9c0eb746
Parthkomalwad <57681207+Parthkomalwad@users.noreply.github.com>
2022-02-23 23:44:17 +0530

    Added frontend and backend and later will add mongodb

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Router>
+      <Header></Header>
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen></HomeScreen>} exact></Route>
+            <Route
+              path='/product/:id'
+              element={<ProductScreen></ProductScreen>}
+              exact
+            ></Route>
+          </Routes>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </Router>
   )
 }
 
